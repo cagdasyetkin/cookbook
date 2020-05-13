66f102826b952368f91b492b10db2134fed756c8
cagdasyetkin <cagdasyetkin@yahoo.com>
2020-05-13 17:17:59 +0200

    add dict.py

---
+++ dict.py
@@ -0,0 +1,8 @@
+mydictionary = {"lion": 
+                        {"age": 10, "origin": "ZA"},
+                
+                "zebra": 15, 
+                "mouse": 1}
+
+# print the age value of lion in the zoo
+print(mydictionary["lion"]["age"])
